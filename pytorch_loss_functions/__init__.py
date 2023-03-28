a79d1522e45dcb345db4bad85bdb65fbba77a422
ashishdatta <ashishbdatta@gmail.com>
2023-03-27 20:35:04 -0700

    Added Dice and BCE+DICE

---
+++ pytorch_loss_functions/__init__.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class FocalLoss():
+    pass
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+    
+    def forward(self, y_pred, y_true, smooth=1):
+        y_pred = F.sigmoid(y_pred)
+
+        y_pred = y_pred.view(-1)
+        y_true = y_true.view(-1)
+
+        intersection = (y_pred * y_true).sum()
+        dice = (2. * intersection + smooth) / (y_pred.sum() + y_true.sum() + smooth)
+        return 1 - dice
+
+class DiceBCELoss():
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, y_pred, y_true, smooth):
+
+        y_pred = F.sigmoid(y_pred)
+
+        y_pred = y_pred.view(-1)
+        y_true = y_true.view(-1)
+        intersection = (y_pred * y_true).sum()
+        dice = 1 - (2. * intersection + smooth) / (y_pred.sum() + y_true.sum() + smooth)
+        BCE = F.binary_cross_entropy(y_pred, y_true, reduction='mean')
+        dice_bce = BCE + dice
+
+        return dice_bce
+
+class IoULoss():
+    pass
+
+class TverskyLoss():
+    pass
+
+class FocalTverskyLoss():
+    pass
+
+class LovaszHingeLoss():
+    pass
+
+class ComboLoss():
+    pass
