3a810d3b004b36733e1431a24a4f108f77a0bb85
ashishdatta <ashishbdatta@gmail.com>
2023-03-28 12:41:42 -0700

    Made code a little cleaner by adding type hints and running python formatter

--- pytorch_loss_functions/__init__.py
+++ pytorch_loss_functions/__init__.py
@@ -2,61 +2,78 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-
+from typing import Optional
 
 
 class FocalLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(FocalLoss, self).__init__()
 
-    def forward(self, y_pred, y_true, alpha=0.8, gamma=2, smooth=1):
+    def forward(
+        self,
+        y_pred: torch.Tensor,
+        y_true: torch.Tensor,
+        alpha: Optional[float] = 0.8,
+        gamma: Optional[float] = 2.0,
+        smooth: Optional[int] = 1,
+    ) -> torch.Tensor:
         y_pred = F.sigmoid(y_pred)
         y_pred = y_pred.view(-1)
         y_true = y_true.view(-1)
 
-        logpt = F.binary_cross_entropy(y_pred, y_true, reduction='mean')
+        logpt = F.binary_cross_entropy(y_pred, y_true, reduction="mean")
         pt = torch.exp(-logpt)
         focal_term = (1.0 - pt).pow(gamma)
         focal_loss = alpha * focal_term * logpt
-        
-        return  focal_loss
+
+        return focal_loss
+
 
 class DiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(DiceLoss, self).__init__()
-    
-    def forward(self, y_pred, y_true, smooth=1):
+
+    def forward(
+        self, y_pred: torch.Tensor, y_true: torch.Tensor, smooth: Optional[int] = 1
+    ) -> torch.Tensor:
         y_pred = F.sigmoid(y_pred)
 
         y_pred = y_pred.view(-1)
         y_true = y_true.view(-1)
 
         intersection = (y_pred * y_true).sum()
-        dice = (2. * intersection + smooth) / (y_pred.sum() + y_true.sum() + smooth)
+        dice = (2.0 * intersection + smooth) / (y_pred.sum() + y_true.sum() + smooth)
         return 1 - dice
+
 
 class DiceBCELoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(DiceBCELoss, self).__init__()
 
-    def forward(self, y_pred, y_true, smooth=1):
-
+    def forward(
+        self, y_pred: torch.Tensor, y_true: torch.Tensor, smooth: Optional[int] = 1
+    ) -> torch.Tensor:
         y_pred = F.sigmoid(y_pred)
 
         y_pred = y_pred.view(-1)
         y_true = y_true.view(-1)
         intersection = (y_pred * y_true).sum()
-        dice = 1 - (2. * intersection + smooth) / (y_pred.sum() + y_true.sum() + smooth)
-        BCE = F.binary_cross_entropy(y_pred, y_true, reduction='mean')
+        dice = 1 - (2.0 * intersection + smooth) / (
+            y_pred.sum() + y_true.sum() + smooth
+        )
+        BCE = F.binary_cross_entropy(y_pred, y_true, reduction="mean")
         dice_bce = BCE + dice
 
         return dice_bce
+
 
 class IoULoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(IoULoss, self).__init__()
 
-    def forward(self, y_pred, y_true, smooth=1):
+    def forward(
+        self, y_pred: torch.Tensor, y_true: torch.Tensor, smooth: Optional[int] = 1
+    ) -> torch.Tensor:
         y_pred = F.sigmoid(y_pred)
 
         y_pred = y_pred.view(-1)
@@ -67,30 +84,41 @@ class IoULoss(nn.Module):
         IoU = (intersection + smooth) / (union + smooth)
         return 1 - IoU
 
+
 # Note when alpha and beta = 0.5 this is the same as Dice Loss
 class TverskyLoss(nn.Module):
     def __init__(self, weights=None, size_average=True):
         super(TverskyLoss, self).__init__()
 
-    def forward(self, y_pred, y_true, smooth=1, alpha=0.5, beta=0.5):
+    def forward(
+        self,
+        y_pred: torch.Tensor,
+        y_true: torch.Tensor,
+        smooth: Optional[int] = 1,
+        alpha: Optional[float] = 0.5,
+        beta: Optional[float] = 0.5,
+    ) -> torch.Tensor:
         y_pred = F.sigmoid(y_pred)
 
         y_pred = y_pred.view(-1)
         y_true = y_true.view(-1)
 
         TP = (y_pred * y_true).sum()
-        FP = ((1-y_true) * y_pred).sum()
+        FP = ((1 - y_true) * y_pred).sum()
         FN = (y_true * (1 - y_pred)).sum()
 
         tversky = (TP + smooth) / (TP + alpha * FP + beta * FN + smooth)
 
         return 1 - tversky
 
+
 class FocalTverskyLoss(nn.Module):
     pass
 
+
 class LovaszHingeLoss(nn.Module):
     pass
+
 
 class ComboLoss(nn.Module):
     pass
--- tests/test_segmentation_losses.py
+++ tests/test_segmentation_losses.py
@@ -9,19 +9,28 @@ import segmentation_models_pytorch as smp
 
 from pprint import pprint
 from torch.utils.data import DataLoader
-from pytorch_loss_functions import DiceLoss, DiceBCELoss, IoULoss, FocalLoss, TverskyLoss
+from pytorch_loss_functions import (
+    DiceLoss,
+    DiceBCELoss,
+    IoULoss,
+    FocalLoss,
+    TverskyLoss,
+)
+
 
 def train(model, device, train_loader, optimizer, epoch, loss):
     model.train()
     for batch_idx, batch in enumerate(train_loader):
-        data = batch['image']
-        target = batch['mask']
+        data = batch["image"]
+        target = batch["mask"]
         data, target = data.to(device), target.to(device)
         data = data.float()
         optimizer.zero_grad()
         output = model(data)
         if loss == "dice":
-            loss_fn = DiceLoss() #smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+            loss_fn = (
+                DiceLoss()
+            )  # smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
         elif loss == "dicebce":
             loss_fn = DiceBCELoss()
         elif loss == "IoU":
@@ -34,12 +43,18 @@ def train(model, device, train_loader, optimizer, epoch, loss):
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
-             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                 100. * batch_idx / len(train_loader), loss.item()))
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
+
 
 def dry_run(loss):
-    
     torch.manual_seed(42)
     use_cuda = torch.cuda.is_available()
     if use_cuda:
@@ -48,8 +63,8 @@ def dry_run(loss):
         device = torch.device("cpu")
     print(device)
     root = "./"
-    #SimpleOxfordPetDataset.download(root)
-    model = smp.Unet('mobilenet_v2', in_channels=3, classes=1).to(device)
+    # SimpleOxfordPetDataset.download(root)
+    model = smp.Unet("mobilenet_v2", in_channels=3, classes=1).to(device)
     train_dataset = SimpleOxfordPetDataset(root, "train")
     valid_dataset = SimpleOxfordPetDataset(root, "valid")
     test_dataset = SimpleOxfordPetDataset(root, "test")
@@ -63,21 +78,32 @@ def dry_run(loss):
     print(f"Test size: {len(test_dataset)}")
 
     n_cpu = os.cpu_count()
-    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
-    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu
+    )
+    valid_dataloader = DataLoader(
+        valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu
+    )
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     train(model, device, train_dataloader, optimizer, 0, loss)
 
-class TestSegmentationLosses():
+
+class TestSegmentationLosses:
     def test_dice_loss(self):
         dry_run("dice")
+
     def test_dicebce_loss(self):
         dry_run("dicebce")
+
     def test_iou_loss(self):
         dry_run("IoU")
+
     def test_focal_loss(self):
         dry_run("focal")
+
     def test_tversky_loss(self):
         dry_run("tversky")
